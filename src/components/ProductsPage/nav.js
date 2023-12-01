05f491561f60c1b4fe381dff88e9dbe92c55ac1b
GangHyeok Lee <rkdgur5381@gmail.com>
2023-12-01 11:47:03 +0900

    배너, 아이템, 네비게이션

--- src/components/ProductsPage/nav.js
+++ src/components/ProductsPage/nav.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import "./nav.css"
+import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { GoPerson } from 'react-icons/go'
+import { ImExit } from 'react-icons/im'
 
 function Nav() {
   return (
     <nav className="nav">
-      test
+      <div className="shopping" onClick={() => window.location.reload()}>
+        Shop
+      </div>
+      <div className="nav_buttons">
+        <AiOutlineShoppingCart className="button"/>
+        <GoPerson className="button"/>
+        <ImExit className="button"/>
+      </div>
     </nav>
   )
 }
